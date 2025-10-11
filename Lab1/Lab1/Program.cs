3032b0dc9e66c2f9a13c5b855dfd9f689d73cc47
Kacper Stachowicz <kstachowicz@edu.cdv.pl>
2025-10-11 16:24:46 +0200

    Lab1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.ComponentModel.Design;
 
-Console.WriteLine("Hello, World!");
+const int requiredAge = 14;
+const int beerRequiredAge = 18;
+const int simRequiredAge = 18;
+const string accessDeniedMessage = "You must be at least 16 years old.";
+const string accessAllowedMessage = "Welcome to my shop.";
+const string beerRestrictionMessage = "You must be 18 years old to buy a beer.";
+const string simRestrictionMessage = "You must be 18 years old to register a SIM Card.";
+
+int age = 16;
+
+if (age >= beerRequiredAge && age >=simRequiredAge)
+{
+    Console.WriteLine(accessAllowedMessage);
+}
+else if (age >= requiredAge)
+{
+    Console.WriteLine($"{accessAllowedMessage} {beerRestrictionMessage} {simRestrictionMessage}");
+}
+else
+{
+    Console.WriteLine(accessDeniedMessage);
+}
